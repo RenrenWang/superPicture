f8fca3fda4bb4c69ef8b9bdbf1c6d73834da57e6
“root” <renwang@rendeMacBook-Pro.local>
2022-06-29 00:10:28 +0800

    feat: article

--- app/service/article.ts
+++ app/service/article.ts
@@ -1,7 +1,16 @@
 import { Service } from "egg";
 import { ArticleType } from "../types";
 export default class Article extends Service {
+  public async find() {
+    return this.ctx.model.Article.find();
+  }
   public async create(post: ArticleType) {
     return this.ctx.model.Article.create(post);
+  }
+  public async delete(id: string) {
+    return this.ctx.model.Article.deleteOne({ id });
+  }
+  public async update(post: ArticleType) {
+    return this.ctx.model.Article.updateOne(post);
   }
 }
